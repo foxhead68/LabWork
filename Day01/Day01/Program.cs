b409d3db8965fc586c0e0b42c4223e25f31a895d
Garrett Girod <girodg@hotmail.com>
2022-06-27 10:30:33 -0400

    getmessage

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,6 +6,27 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            int result = Add(15, 10);
+            PrintMessage();
+            string callSign = GetMessage();
+        }
+
+        static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello Gotham!");
+        }
+
+        static string GetMessage()
+        {
+            Console.Write("What is your call sign? ");
+            string msg = Console.ReadLine();
+            return msg;
         }
 
     }
